a5572106d983fe73d0ec4f6eaee4f9260f5beb2a
Valéria T.S <valeria_signor@estudante.sesisenai.org.br>
2024-06-25 14:55:10 -0300

    exercicio 5

---
+++ LISTA 01/EXERCICIO 5/index.js
@@ -0,0 +1,13 @@
+let botao1 = document.getElementById("botao1")
+let botao2 = document.getElementById("botao2")
+botao1.addEventListener("click", function() {
+     
+document.getElementById("saudacao").style.display = ("block");
+
+})
+
+botao2.addEventListener("click", function() {
+    
+document.getElementById("saudacao").style.display = ("none");
+
+})
